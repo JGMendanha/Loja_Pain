fb9d61430daaea2568d09ffee56e433c2d092054
João Guilherme Mendanha Alves <alvesmega20@gmail.com>
2025-03-31 20:22:26 -0300

    Product page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { ProductType } from "@/types/ProductType";
 import Product from "./components/Product";
 import Stripe from "stripe";
+import ImageCarousel from "./components/ImageCarousel";
 
 async function getProducts(): Promise<ProductType[]>{
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
@@ -32,18 +33,22 @@ export default async function Home() {
   const products = await getProducts();
 
   return (
-    <div className="max-w-8xl mx-auto pt-8 px-8">
-      <div className="grid grid-cols-1
-      sm:grid-cols-2 
-      md:grid-cols-3 
-      lg:grid-cols-4 
-      gap-10 xl:gap-6">
+    <>
+      <ImageCarousel />
+      <div className="max-w-8xl mx-auto pt-8 px-8">
+        <div className="grid grid-cols-1
+        sm:grid-cols-2 
+        md:grid-cols-3 
+        lg:grid-cols-4 
+        gap-10 xl:gap-6">
 
-        {products.map((product) => {
-          return <Product key={product.id} product={product} />
-        })}
+          {products.map((product) => {
+            return <Product key={product.id} product={product} />
+          })}
 
+        </div>
       </div>
-    </div>
+    </>
+    
   )
 }
---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,50 @@
+import ProductImage from "@/app/components/ProductImage";
+import Stripe from "stripe";
+import { formatPrice } from "@/lib/utils";
+import AddCart from "@/app/components/addCart";
+
+type ProductPageParams = {
+    params:{
+        id: string;
+    };
+};
+
+async function getProduct(id: string) {
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+        apiVersion: "2025-02-24.acacia"
+    })
+
+    const product = await stripe.products.retrieve(id);
+
+    const price = await stripe.prices.list({
+        product: product.id,
+    });
+
+    return {
+        id: product.id,
+        price: price.data[0].unit_amount,
+        name: product.name,
+        image: product.images[0],
+        description: product.description,
+        currency: price.data[0].currency,
+    };
+}
+
+export default async function ProductPage({params: {id}}: ProductPageParams ){
+    const product = await getProduct(id);
+    return(
+        <div className="flex flex-col md:flex-row items-center max-w-7xl mx-auto gap-8 p-10 bg-slate-950">
+            <ProductImage product={product} />
+            <div className="flex flex-col">
+                <div className="pb-4">
+                    <h1 className="text-2xl font-bold text-slate-100">{product.name}</h1>
+                    <h2 className="text-2xl font-bold text-slate-100">{formatPrice(product.price)}</h2>
+                </div>
+                <div className="pb-4">
+                    <p className="text-slate-100 text-md">{product.description}</p>
+                </div>
+                <AddCart product={product}/>
+            </div>
+        </div>
+    )
+}
