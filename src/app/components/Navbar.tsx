393909ca509df043b3f2d145bd86fff6f5953036
João Guilherme Mendanha Alves <alvesmega20@gmail.com>
2025-03-31 00:48:39 -0300

    Auth configuration

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,31 +1,48 @@
-import Link from 'next/link'
+"use client";
+
+import Link from 'next/link';
 import logo from '@/app/images/logo.png';
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
 
-function Navbar(){
-    return(
-        <nav className='fixed 
-        top-0 
-        w-full 
-        flex items-center 
-        py-2 px-8 z-50
-        justify-between 
-        bg-slate-950
-        text-gray-100'>
+export default function Navbar() {
+  
+  return (
+    <nav className='fixed 
+      top-0 
+      w-full 
+      flex items-center 
+      py-2 px-8 z-50
+      justify-between 
+      bg-slate-950
+      text-gray-100'>
 
-          <Link className='text-nd h-12 font-extrabold
-          flex items-center' href="/">
+      <Link className='text-nd h-12 font-extrabold
+        flex items-center' href="/">
 
-          <img
-            src={logo.src} // Acessa a URL processada
-            alt="Logo Pain Gaming"
-            className="h-8 mr-2"
-          />
-            paiN Gaming
+        <img
+          src={logo.src}
+          alt="Logo Pain Gaming"
+          className="h-8 mr-2"
+        />
+        paiN Gaming
+      </Link>
 
-          </Link>
+      <div className='flex items-center gap-8'>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
 
-        </nav>
-    )
+        <SignedOut>
+          <SignInButton mode='modal'>
+          </SignInButton>
+        </SignedOut>
+      </div>
+
+    </nav>
+  );
 }
-
-export default Navbar
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 import Navbar from './components/Navbar'
 import ImageCarousel from './components/ImageCarousel'
 import { ClerkProvider } from '@clerk/nextjs'
-
+import { ptBR } from '@clerk/localizations'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,9 +19,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider localization={ptBR}>
       <html lang="en">
-        <body className={clsx(inter.className, 'bg-slate-100')}>
+        <body className={clsx(inter.className, `bg-slate-100`)}>
           <Navbar />
           <main className='h-screen p-16'>
             <ImageCarousel />
