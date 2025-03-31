fcf7843f49af6dc621f6960e90adbfb249ef2efb
João Guilherme Mendanha Alves <alvesmega20@gmail.com>
2025-03-31 18:23:17 -0300

    global store

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -8,8 +8,11 @@ import {
   SignedOut,
   UserButton,
 } from '@clerk/nextjs';
+import { useCartStore } from '@/store';
 
 export default function Navbar() {
+
+  const useStore = useCartStore()
   
   return (
     <nav className='fixed 
@@ -32,15 +35,34 @@ export default function Navbar() {
         paiN Gaming
       </Link>
 
-      <div className='flex items-center gap-8'>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
+      <div className='flex items-center gap-10'>
 
-        <SignedOut>
-          <SignInButton mode='modal'>
-          </SignInButton>
-        </SignedOut>
+        <div className='flex items-center cursor-pointer relative'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <circle cx="9" cy="21" r="1"></circle>
+            <circle cx="20" cy="21" r="1"></circle>
+            <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+          </svg>
+          <span className='bg-slate-100 text-slate-950 
+          flex items-center justify-center absolute 
+          left-6 bottom-3  
+          text-sm font-bold rounded-full 
+          h-5 w-5'>
+            2
+          </span>
+        </div>
+
+        <div>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton mode='modal'>
+            </SignInButton>
+          </SignedOut>
+        </div>
+
       </div>
 
     </nav>
