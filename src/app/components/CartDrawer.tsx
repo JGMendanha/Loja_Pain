a8921c4e2e050a1bcd1300f38777ae37a24a58b7
João Guilherme Mendanha Alves <alvesmega20@gmail.com>
2025-04-01 00:37:29 -0300

    payment intent

--- src/app/components/CartDrawer.tsx
+++ src/app/components/CartDrawer.tsx
@@ -56,6 +56,7 @@ export default function cartDrawer(){
 
                 {
                     useStore.onCheckout === 'checkout' && (
+                        
                         <Checkout/>
                     )
                 }
--- src/store.ts
+++ src/store.ts
@@ -10,6 +10,8 @@ type CartState = {
     toggleCart: () => void;
     onCheckout: string;
     setCheckout: (checkout: string) => void;
+    paymentIntent: string;
+    setPaymentIntent: (paymentIntent: string) => void;
 };
 
 export const useCartStore = create<CartState>()(
@@ -49,6 +51,8 @@ export const useCartStore = create<CartState>()(
         isOpen: false,
         toggleCart: () => set((state) => ({isOpen: !state.isOpen})),
         onCheckout: 'cart',
-        setCheckout: (checkout) => set(() => ({onCheckout: checkout}))
+        setCheckout: (checkout) => set(() => ({onCheckout: checkout})),
+        paymentIntent: '',
+        setPaymentIntent: (paymentIntent) => set(() => ({paymentIntent})),
     }), {name: 'cart-storage'})
 );
