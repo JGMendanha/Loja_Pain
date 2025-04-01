4606dab69abfd015828b6c423da32d3b932e2cf7
João Guilherme Mendanha Alves <alvesmega20@gmail.com>
2025-03-31 21:50:07 -0300

    Link home page to product page

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -2,30 +2,33 @@ import { ProductType } from "@/types/ProductType"
 import ProductImage from "./ProductImage"
 import { formatPrice } from "@/lib/utils"
 import AddCart from "./addCart"
+import Link from "next/link"
 
 type ProductProps = {
     product: ProductType 
 }
 
 export default function product({product} : ProductProps){
-    return(
+    return( 
+        
         <div className="flex flex-col shadow-lg h-96 bg-slate-800 p-5 ">
+ 
+                    <div className="relative max-h-72 flex-1 text-gray-100">
+                        <ProductImage product={product} fill/>
+                    </div>
 
-            <div className="relative max-h-72 flex-1 text-gray-100">
-                <ProductImage product={product} fill/>
-            </div>
-
-            <div className="flex justify-between font-bold my-3 text-gray-100">
-                <p className="w-40 truncate">
-                    {product.name}
-                </p>
-                <p className="text-md text-slate-100">
-                    ${formatPrice(product.price)}
-                </p>
-            </div>
+                    <Link href={`/product/${product.id}`}>
+                        <div className="flex justify-between font-bold my-3 text-gray-100">
+                            <p className="w-40 truncate">
+                                {product.name}
+                            </p>
+                            <p className="text-md text-slate-100">
+                                ${formatPrice(product.price)}
+                            </p>
+                        </div>
+                    </Link>
 
             <AddCart product={product} />
-
         </div>
     )
 }
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -33,18 +33,21 @@ async function getProduct(id: string) {
 export default async function ProductPage({params: {id}}: ProductPageParams ){
     const product = await getProduct(id);
     return(
-        <div className="flex flex-col md:flex-row items-center max-w-7xl mx-auto gap-8 p-10 bg-slate-950">
-            <ProductImage product={product} />
-            <div className="flex flex-col">
-                <div className="pb-4">
-                    <h1 className="text-2xl font-bold text-slate-100">{product.name}</h1>
-                    <h2 className="text-2xl font-bold text-slate-100">{formatPrice(product.price)}</h2>
+        <div className="pt-16">
+            <div className="flex flex-col md:flex-row items-center max-w-4xl mx-auto gap-8 p-10 bg-slate-950">
+                <ProductImage product={product} />
+                <div className="flex flex-col">
+                    <div className="pb-4">
+                        <h1 className="text-2xl font-bold text-slate-100">{product.name}</h1>
+                        <h2 className="text-2xl font-bold text-slate-100">{formatPrice(product.price)}</h2>
+                    </div>
+                    <div className="pb-4">
+                        <p className="text-slate-100 text-md">{product.description}</p>
+                    </div>
+                    <AddCart product={product}/>
                 </div>
-                <div className="pb-4">
-                    <p className="text-slate-100 text-md">{product.description}</p>
-                </div>
-                <AddCart product={product}/>
             </div>
         </div>
+        
     )
 }
